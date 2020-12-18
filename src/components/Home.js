4ee2d328e42161ae7c2adbd64322e3679d22022a
jd <duverseaujohnny21@gmail.com>
2020-12-18 12:19:47 -0500

    create a home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+  <React.Fragment>
+    <h1 className="center-align">Caribbean Covid-19 Tracker</h1>
+
+  </React.Fragment>
+  )
+}
+
+export default Home;
