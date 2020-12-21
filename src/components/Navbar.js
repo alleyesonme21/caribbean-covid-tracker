8026da578f539057b7ebbb8c93db29d76bd66b44
jd <duverseaujohnny21@gmail.com>
2020-12-21 17:30:31 -0500

    import navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,125 @@
-// import React from 'react';
-// import { link } from 'react-router-dom';
-// // import './Navbar';
+// import React, { useState, useEffect } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+//  import { Button } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+// import './Navbar.css';
 
 // function Navbar() {
+//   const [click, setClick] = useState(false);
+//   const [button, setButton] = useState(true);
+
+//   const handleClick = () => setClick(!click);
+//   const closeMobileMenu = () => setClick(false);
+
+//   const showButton = () => {
+//     if (window.innerWidth <= 960) {
+//       setButton(false);
+//     } else {
+//       setButton(true);
+//     }
+//   };
+
+//   useEffect(() => {
+//     showButton();
+//   }, []);
+
+//   window.addEventListener('resize', showButton);
+
 //   return (
 //     <React.Fragment>
-//     <nav className="navbar">
-//       <div className="navbar-container">
-//         <link to="/" className="navbar-logo" /> 
-//         Caribbean Globe <i class="fas fa-globe-americas"></i>
-//       </div>
-//     </nav>
-//   </React.Fragment>
-//   )
+//       <nav className='navbar'>
+//         <div className='navbar-container'>
+//           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+//             TRVL
+//             <i class='fab fa-typo3' />
+//           </Link>
+//           <div className='menu-icon' onClick={handleClick}>
+//             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+//           </div>
+//           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+//             <li className='nav-item'>
+//               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+//                 Home
+//               </Link>
+//             </li>
+//             <li className='nav-item'>
+//               <Link
+//                 to='/services'
+//                 className='nav-links'
+//                 onClick={closeMobileMenu}
+//               >
+//                 Services
+//               </Link>
+//             </li>
+//             <li className='nav-item'>
+//               <Link
+//                 to='/products'
+//                 className='nav-links'
+//                 onClick={closeMobileMenu}
+//               >
+//                 Products
+//               </Link>
+//             </li>
+
+//             <li>
+//               <Link
+//                 to='/sign-up'
+//                 className='nav-links-mobile'
+//                 onClick={closeMobileMenu}
+//               >
+//                 Sign Up
+//               </Link>
+//             </li>
+//           </ul>
+//           {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+//         </div>
+//       </nav>
+//     </React.Fragment>
+//   );
 // }
 
 // export default Navbar;
 
+
+
+
+
+import React from 'react';
+import {  Nav, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+  return (
+  <React.Fragment>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">About</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Destinations</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+
+
+    {/* <h1 className="center-align">Caribbean Covid-19 Tracker</h1> */}
+  </React.Fragment>
+  )
+}
+
+export default Navbar;
